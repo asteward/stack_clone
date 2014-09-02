096ae5ce9fcc7dcbc18ddf75dcc96b4b65318e1b
Austin Steward & J.J. Idt <student@epicodus.com>
2014-09-02 10:45:33 -0700

    add user signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Thanks for signing up, #{@user.name}!"
+      redirect_to :back
+    else
+      redirect_to "signup"
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
